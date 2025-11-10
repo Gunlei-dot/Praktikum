7f4f76ab8509d172b90d1cc3ec7d512de3407be9
Keanu Walters <keanuwalters01@gmail.com>
2025-11-10 12:23:56 +0100

    optimise data split and data types, adjust to test forest parms

--- testing_comparison.py
+++ testing_comparison.py
@@ -5,6 +5,7 @@ import mlflow
 import mlflow.sklearn
 from mlflow.models import infer_signature
 from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 #from sklearn.model_selection import train_test_split #function currently not used
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, precision_score, recall_score, f1_score  # metrics for manual model eval
@@ -15,22 +16,22 @@ from sklearn.model_selection import cross_val_score
 
 patient_data = pd.read_csv(r'C:\Users\s434037\Desktop\Bachelor\projects\labels.tsv', encoding='utf-8', sep='\t') #encoding and sep to read tsv correctly
 patient_data = patient_data.dropna() # Drop rows with missing values for simplicity 
-patient_data = patient_data.drop(columns=['pseudo_id']) # Drop patient_id as it's not a feature for prediction
-patient_data = patient_data.drop(columns=['sex']) # Drop sex as it's not a feature for prediction
-patient_data = patient_data.drop(columns=['pseudo_patid']) # Drop pst_id as it's not a feature for prediction
-patient_data = patient_data.drop(columns=['set']) # Drop the set indicator columns
+patient_data = patient_data.drop(columns=['pseudo_id', 'sex', 'pseudo_patid']) # Drop patient_id as it's not a feature for prediction
 patient_data = patient_data[patient_data.label != 2] # Remove rows with label 2 as these are not relevant for binary classification
 patient_data = patient_data[patient_data.psa != 'NA'] # remove rows with no psa value till i find a better solution
 patient_data = patient_data[patient_data.staging != 'primary'] # remove rows with primary staging till i find a better solution
+
 patient_data['age'] = patient_data['age'].astype(float) # convert psa to float
 patient_data['px'] = patient_data['px'].astype(float) # convert psa to float
 
-X = pd.get_dummies(patient_data.drop("label", axis=1)) # dummies for categorical variables since DecisionTree doesn't handle them directly
-y = pd.get_dummies(patient_data.drop(columns=["age", "staging", "px", "psa"])) # Features and target variable
+train_mask = patient_data['set'] == 'train' 
+test_mask = patient_data['set'] == 'val' # splitting train/val before one-hot encoding
 
-#X_train, X_test, y_train, y_test =train_test_split(X, y, test_size=0.2, random_state=42)  (#random_state for reproducibility, same split)
+X = pd.get_dummies(patient_data.drop("label", axis=1)) # dummies for categorical variables since forest doesn't handle them directly
+X.index = patient_data.index
+y = patient_data[["label"]].astype(int) # Keeping y as DataFrame for easier handling of set indicators
 
-tree_params = {
+'''forest_params = {
     "criterion": "gini",
     "splitter": "best",
     "max_depth": None,
@@ -51,41 +52,39 @@ tree_params = {
 tree_test = tree.DecisionTreeClassifier(**tree_params)
 
 scores = cross_val_score(tree_test, X, y, cv=5)
-print(f"Cross-validation scores: {scores}")
+print(f"Cross-validation scores: {scores}")''' # section used for testing cross validation before implementing grid search
 
-'''X_train =   X[X.set_train == True]
-X_test =    X[X.set_val == True]
-y_train =   y[y.set_train == True]
-y_test =    y[y.set_val == True]
+X_train, y_train = X[train_mask], y[train_mask]
+X_test, y_test = X[test_mask], y[test_mask]
+
+X_train = X_train.drop(columns=['set_train', 'set_val'], errors= 'ignore') # Drop the set indicator columns
+X_test = X_test.drop(columns=['set_train', 'set_val'], errors = 'ignore')
+
+y_test = np.array(y_test).astype(int) # Convert y_test to a NumPy array of strings
+y_train = np.array(y_train).astype(int) # Convert y_train to a NumPy array of strings
+
+y_test = y_test.squeeze()
+y_train = y_train.squeeze() # sections sets up train test split and ensures proper data types
 
 param_grid = {
-    'criterion': ['log_loss'],
-    'max_depth': [None, 10, 20, 30],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4],
-    'min_weight_fraction_leaf': [0.0, 0.1, 0.2],
-    'min_impurity_decrease': [0.0, 0.1, 0.2],
-    'ccp_alpha': [0.0, 0.01, 0.1]
-}'''
+    'min_samples_split': [2, 3, 4],
+    'min_weight_fraction_leaf': [0.1],
+    'min_impurity_decrease': [0.0, 0.01, 0.1],
+    'max_leaf_nodes': [None, 10, 20],
+}
 
-'''# Dropping the set indicator columns after the split
-X_train = X_train.drop(columns=['set_train', 'set_val']) # Drop the set indicator columns
-X_test = X_test.drop(columns=['set_train', 'set_val']) # Drop the set indicator columns
-y_test = y_test.drop(columns=['set_train', 'set_val']) # Drop the set indicator columns
-y_train = y_train.drop(columns=['set_train', 'set_val']) # Drop the set indicator columns
-y_test = np.array(y_test).astype(str) # Convert y_test to a NumPy array of strings
-y_train = np.array(y_train).astype(str) # Convert y_train to a NumPy'''
+# Dropping the set indicator columns after the split
 
-'''with mlflow.start_run(run_name='decision_tree_param_tuning'): # Start an MLflow run to log parameters, metrics, and the model
-    dt_classifier = tree.DecisionTreeClassifier(random_state=42)
-    grid_search = GridSearchCV(estimator=dt_classifier, param_grid=param_grid, cv=5, n_jobs=-1, scoring='accuracy')
+with mlflow.start_run(run_name='random_forest_param_tuning'): # Start an MLflow run to log parameters, metrics, and the model
+    rf_classifier = RandomForestClassifier(random_state=42)
+    grid_search = GridSearchCV(estimator=rf_classifier, param_grid=param_grid, cv=5, n_jobs=-1, scoring='f1', error_score='raise')
     grid_search.fit(X_train, y_train)
     best_params = grid_search.best_params_
     best_score = grid_search.best_score_
-    model = tree.DecisionTreeClassifier(**best_params)
+    model = RandomForestClassifier(**best_params)
     model = model.fit(X_train, y_train)
 
     
-    print(f"Best parameters: {grid_search.best_params_}")
-    print(f"Best cross-validation score: {grid_search.best_score_:.3f}")
-    print(f"Test score: {best_score:.3f}")'''
+print(f"Best parameters: {grid_search.best_params_}")
+print(f"Best cross-validation score: {grid_search.best_score_:.3f}")
+print(f"Test score: {best_score:.3f}")
